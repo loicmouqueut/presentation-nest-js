6874280b796423d4e4967ae8551ff50f7448f6ac
Loïc Mouquet <loic.mouquet@adeo.com>
2026-03-20 00:19:09 +0100

    2 add card ressource

---
+++ src/cards/entities/card.entity.ts
@@ -0,0 +1,14 @@
+export enum Rarity {
+  COMMON = 'common',
+  RARE = 'rare',
+  MYTHIC = 'mythic',
+  LEGENDARY = 'legendary',
+}
+
+export class Card {
+  id: string;
+  name: string;
+  description: string;
+  cost: number;
+  rarity: Rarity;
+}
