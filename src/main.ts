9d674dfff3f6f53a502dae989dfeee5b9bc8aaf4
Loïc Mouquet <loic.mouquet@adeo.com>
2026-03-20 11:37:30 +0100

    4 - add middleware logger and execution interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExecutionInterceptor } from './interceptor/executionInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(
+
+  app.useGlobalInterceptors(new ExecutionInterceptor())
+  app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Remove properties that do not have any decorators
       forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
